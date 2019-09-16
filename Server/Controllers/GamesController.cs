61511b848d0072c4541acca097575089eff5dd4e
IvanAnikin <13561+IvanAnikin@users.noreply.github.com>
2019-09-16 15:28:33 +0200

    Added games page, with buton on default page and one game - flappyKube :-)

---
+++ Server/Controllers/GamesController.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult Get(string chatName)
+        {
+
+            string text;
+            var fileStream = new FileStream(@"GamesPage.html", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                text = streamReader.ReadToEnd();
+            }
+
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = text
+            };
+        }
+    }
+}
