4ec721fb9d8d52668857871fbb48b9162b72f5aa
IvanAnikin <13561+IvanAnikin@users.noreply.github.com>
2019-12-03 17:30:19 +0100

    added GetHash function - fior images compression to compare

--- Server/ChatManager.cs
+++ Server/ChatManager.cs
@@ -9,6 +9,7 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 
 using Microsoft.WindowsAzure.Storage.Blob;
+using System.Drawing;
 
 namespace Server
 {
@@ -431,8 +432,22 @@ namespace Server
 
 
 
-
-
+        //FOR COMPRESSING IMAGES TO COMPARE THEM -- IN MLimages solution
+        public static List<bool> GetHash(Bitmap bmpSource)
+        {
+            List<bool> lResult = new List<bool>();
+            //create new image with 16x16 pixel
+            Bitmap bmpMin = new Bitmap(bmpSource, new Size(16, 16));
+            for (int j = 0; j < bmpMin.Height; j++)
+            {
+                for (int i = 0; i < bmpMin.Width; i++)
+                {
+                    //reduce colors to true / false                
+                    lResult.Add(bmpMin.GetPixel(i, j).GetBrightness() < 0.5f);
+                }
+            }
+            return lResult;
+        }
 
 
 
